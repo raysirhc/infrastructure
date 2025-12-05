2bfa69bd3dfe641b99604dda214eb0e2bd268afa
Fred <fred@example.com>
2025-12-05 08:23:23 +0000

    final

--- workshop03/deploy/variables.tf
+++ workshop03/deploy/variables.tf
@@ -18,25 +18,13 @@ variable DO_IMAGE {
    default = "24.04.ubuntu.x64"
 }
 
-# Add these variables for Ansible
 variable "ansible_user" {
   description = "SSH user for Ansible"
   type        = string
-  default     = "root"
+  default = "root"
 }
 
 variable "ssh_private_key_path" {
   description = "Path to SSH private key for Ansible"
   type        = string
 }
-
-# variable "code_server_password" {
-#   description = "Password for code-server"
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "code_server_version" {
-#   description = "Code Server Version"
-#   type        = string
-# }
